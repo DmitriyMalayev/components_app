94e87729d8a926deeb36b0dd78e4a67363e6f043
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-17 20:46:19 -0400

    duplicated code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker"
+
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img src="" alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Dmitriy
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 9:00 PM</span>
+          </div>
+          <div className="text">Nice Blog Post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Dmitriy
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 9:00 PM</span>
+          </div>
+          <div className="text">Nice Blog Post!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
