5e678d8073ac34ac685a315708391393e8417c0e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-17 20:35:22 -0400

    added static info

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div> Hello World! </div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src="" alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Dmitriy
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 9:00 PM</span>
+          </div>
+          <div className="text">Nice Blog Post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
