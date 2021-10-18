ec6a6ac8a0b1efa46a00c5f4c2c1967b0beee592
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-18 02:11:16 -0400

    added props with faker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        comment="Nice post"
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 4:45PM"
+        comment="Great post"
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 4:45PM"
+        comment="Awesome post"
+        image={faker.image.avatar()}
+      />
     </div>
   );
 };
